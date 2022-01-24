51348e44a8b15e14128d4be2a4630ba8fddbb840
dannyrgd <danny.ramirezgd@gmail.com>
2022-01-24 12:36:26 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+//Generate a webpage that displays my team's basic info
+
+//create a prompt that asks the user several questions
+//first questions: team manager's name, employee ID, email address, office number
+//ask user if they want to add engineer or intern or finish building
+//for Engineer: name, ID, email, github username, then back to menu
+//for Intern: name, ID, email, school, take back to menu
+//capture info into html page
+
+//application should use jest and inquirer
+
+//application must have these classes
+//Employee Manager Engineer Intern
